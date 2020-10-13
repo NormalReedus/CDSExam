1ef4d65381307d9b4dca7bc0bcc31e6dad84bb33
Magnus <magnus.borregaard@gmail.com>
2020-10-13 16:32:52 +0200

    print all geoIds

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['@/assets/_theme.css'],
+  css: ['@/assets/_theme.css', '@/assets/_base.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
