94350b6cfcb1f387f5c69680476ba0b537732fac
CrispyNipsy <magnus.borregaard@gmail.com>
2020-10-13 19:12:06 +0200

    done

--- nuxt/components/World.vue
+++ nuxt/components/World.vue
@@ -1,5 +1,8 @@
 <template>
-  <WorldSvg />
+  <div class="map-container">
+    <h1 class="map-title">Year 3000</h1>
+    <WorldSvg />
+  </div>
 </template>
 
 <script>
@@ -7,4 +10,13 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+.map-container {
+}
+
+.map-title {
+  margin-bottom: 2rem;
+
+  font-family: var(--heading-font);
+  font-weight: var(--heading-weight);
+}
 </style>
