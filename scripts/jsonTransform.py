9366d386ea66ba4070ceaeda21cc50cd4fe486b3
Mikkel Ottosen <mikkel.mokasin@gmail.com>
2020-10-14 23:12:28 +0200

    Added a function in jsonTransform.py that finds max deaths and cases

--- scripts/jsonTransform.py
+++ scripts/jsonTransform.py
@@ -25,6 +25,12 @@ def get_unique_dates(covid19_dict):
         
     return used_dates
 
+def get_max_deaths_and_cases(covid19_dict):
+    deaths = [entry['deaths'] for entry in covid19_dict]
+    cases = [entry['cases'] for entry in covid19_dict]
+
+    return (max(deaths), max(cases))
+
 #The url from which the json is downloaded
 covid19_data_url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
 
@@ -45,6 +51,9 @@ with urllib.request.urlopen(covid19_data_url) as covid_19_json:
         #When found, the entire chunk of data is appended into the countries key in the dictionary.
         for entry in covid19_dict:
             if entry['dateRep'] == date:
+                entry['deaths_percent'] = calc_deaths(covid19_dict, entry['deaths'])
+                entry['cases_percent'] = calc_cases(covid19_dict, entry['cases'])
+
                 date_dict['records'].append(entry)
 
         #The entire dictionary is then appended to the list.
@@ -53,8 +62,10 @@ with urllib.request.urlopen(covid19_data_url) as covid_19_json:
     #The new data list is transformed into json format.
     data_correct_form_json = json.dumps(data_correct_form)
 
-    #The end folder is located and the file inside is updated with the new information.
-    with open('../nuxt/data/covid_19_worldwide_correct_form.json', 'w') as endpoint:
+    # The end folder is located and the file inside is updated with the new information.
+    with open('../nuxt/data/covid_19.json', 'w') as endpoint:
         endpoint.write(data_correct_form_json)
+
+
 
 
