9f241445ca581f0d4d05cca9aaabc6abc7c9aa2a
Magnus <magnus.borregaard@gmail.com>
2020-10-22 14:53:31 +0200

    finish calc_max_vals

--- scripts/calc_max_values.py
+++ scripts/calc_max_values.py
@@ -6,15 +6,21 @@ this_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(this_dir)
 
 def get_max_values(covid_19_records):
-    deaths = [record['deaths'] for record in covid_19_records]
-    cases = [record['cases'] for record in covid_19_records]
-		
-		deaths_per_pop = [(record['daths'] / record['popData2019']) for record in covid_19_records]
-		cases_per_pop = [(record['cases'] / record['popData2019']) for record in covid_19_records]
+	# Generate lists of all deaths and cases:
+	deaths = [record['deaths'] for record in covid_19_records]
+	cases = [record['cases'] for record in covid_19_records]
 
-    return (max(deaths), max(cases))
+	# Generate lists of deaths and cases per capita (ignoring cases that were not in a geographic area):
+	deaths_per_pop = [(record['deaths'] / record['popData2019']) for record in covid_19_records if record['popData2019'] != None]
+	cases_per_pop = [(record['cases'] / record['popData2019']) for record in covid_19_records if record['popData2019'] != None]
+
+	return { "max_deaths": max(deaths), "max_cases": max(cases), "max_cases_per_pop": max(cases_per_pop), "max_deaths_per_pop": max(deaths_per_pop) }
+
 
 with open('../input_data/covid_19_records.json') as input_file:
-	covid_19_records = json.loads(file.read())['records']
+	covid_19_records = json.loads(input_file.read())['records']
 
 	max_vals = get_max_values(covid_19_records)
+
+	with open('../temporary_data/covid_19_max_values.json', 'w') as output_file:
+		json.dump(max_vals, output_file)
