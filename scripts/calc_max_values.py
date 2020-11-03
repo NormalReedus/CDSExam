7c97bc863a2908aeedd63a714057e970ca569d5d
CrispyNipsy <magnus.borregaard@gmail.com>
2020-11-03 11:34:33 +0100

    .

--- scripts/calc_max_values.py
+++ scripts/calc_max_values.py
@@ -7,23 +7,33 @@ print('Step 3: Calculating max covid-19 cases and deaths...')
 this_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(this_dir)
 
+
 def get_max_vals(covid_19_records):
-	# Generate lists of all deaths and cases:
-	deaths = [record['deaths'] for record in covid_19_records]
-	cases = [record['cases'] for record in covid_19_records]
+  # Generate lists of all deaths and cases:
+  deaths = [record['deaths'] for record in covid_19_records]
+  cases = [record['cases'] for record in covid_19_records]
 
-	# Generate lists of deaths and cases per capita, or 0, if the key does not exist
-	# We are only using the list to generate max vals, so 0 is a fine null value:
-	deaths_per_cap = [record.get('deaths_per_cap', 0) for record in covid_19_records]
-	cases_per_cap = [record.get('cases_per_cap', 0) for record in covid_19_records]
+  # Generate lists of deaths and cases per capita, if the key exists:
+  deaths_per_cap = [record.get('deaths_per_cap')
+                    for record in covid_19_records if record.get('deaths_per_cap')]
+  cases_per_cap = [record.get('cases_per_cap')
+                   for record in covid_19_records if record.get('cases_per_cap')]
 
-	return { "deaths": max(deaths), "cases": max(cases), "cases_per_cap": max(cases_per_cap), "deaths_per_cap": max(deaths_per_cap) }
+  def compare_function(e):
+      return e
+
+  return deaths_per_cap.sort(key=compare_function)
+
+  # return {"deaths": max(deaths), "cases": max(cases), "cases_per_cap": max(cases_per_cap), "deaths_per_cap": max(deaths_per_cap)}
+
 
 # Data has not yet been transformed, since it is easier to loop through before nesting data with dates:
 with open('../data/temp/covid_19_vals_per_cap.json') as input_file:
-	covid_19_records = json.loads(input_file.read())['records']
+  covid_19_records = json.loads(input_file.read())['records']
 
-	max_vals = get_max_vals(covid_19_records)
+  max_vals = get_max_vals(covid_19_records)
 
-	with open('../data/temp/covid_19_max_vals.json', 'w') as output_file:
-		json.dump(max_vals, output_file)
+  print(max_vals)
+
+  # with open('../data/temp/covid_19_max_vals.json', 'w') as output_file:
+  # 	json.dump(max_vals, output_file)
