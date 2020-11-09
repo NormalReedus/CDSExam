e8345a6229e013c6b777e96f7b2fff2843daf8a5
Magnus <magnus.borregaard@gmail.com>
2020-11-09 21:54:07 +0100

    use nbconvert to run nb

--- run.sh
+++ run.sh
@@ -8,19 +8,21 @@
 # Relative paths from the location of this script:
 cd "${BASH_SOURCE%/*}"
 
+# Runs all of the data processing:
+jupyter nbconvert --to notebook --inplace --execute scripts/main.ipynb
 # Order is important:
-python scripts/get_raw_data.py
-python scripts/calc_vals_per_cap.py
-python scripts/calc_tertiles.py
-python scripts/transform_data.py
-python scripts/sort_and_filter_data.py
+#python scripts/get_raw_data.py
+#python scripts/calc_vals_per_cap.py
+#python scripts/calc_tertiles.py
+#python scripts/transform_data.py
+#python scripts/sort_and_filter_data.py
 
 echo 'Step 6: Transferring data to the visualizer...'
 cp data/output/covid_19_output.json nuxt/data/covid_19_output.json
 
-echo 'Step 7: Getting the toolbox...'
-cd nuxt && npm i
+# echo 'Step 7: Getting the toolbox...'
+# cd nuxt && npm i
 
-echo 'Step 8: Running the visualizer...'
-npm run build && npm run start
+# echo 'Step 8: Running the visualizer...'
+# npm run build && npm run start
 
