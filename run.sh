70cf23d2fe56a373b9808b568c2e4044d7ea3c0f
CrispyNipsy <magnus.borregaard@gmail.com>
2020-11-19 16:21:23 +0100

    cleaned nb, added descriptions (nbconvert is complaining)

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@
 # Run this script to execute all of the data processing steps (in order) needed to produce the data that are visualized in the browser
 # ------------------------------------------------------------------------------------------------------------------------------------
 
+echo -e 'Fetching data, processing data, installing dependencies, and running the visualizer.\n\e[32mThis might take a while...\e[39m\n\n'
 
 # Relative paths from the location of this script:
 cd "${BASH_SOURCE%/*}"
@@ -14,11 +15,11 @@ cd "${BASH_SOURCE%/*}"
 # Runs all of the data processing:
 jupyter nbconvert --to notebook --inplace --execute scripts/main.ipynb
 
-echo 'Step 6: Transferring data to the visualizer...'
+# Copies the output of data processing into the visualizer's directory:
 cp data/output/covid_19_final.json nuxt/data/covid_19_data.json
 
-# echo 'Step 7: Getting the toolbox...'
+# Installs the visualizer's dependencies
 cd nuxt && npm i
 
-# echo 'Step 8: Running the visualizer...'
+# Opens the visualizer in your default browser:
 npm run dev
